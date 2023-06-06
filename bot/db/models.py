ebab6a29811737b239b8929a733f7794c5b185a1
dsodx <dsodx@pm.me>
2023-06-06 14:02:45 +0300

    Add models

---
+++ bot/db/models.py
@@ -0,0 +1,13 @@
+from sqlalchemy import BIGINT
+from sqlalchemy.orm import mapped_column, Mapped, DeclarativeBase
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(BIGINT, primary_key=True, autoincrement=False)
+    balance: Mapped[int] = mapped_column(BIGINT, nullable=False, default=0)
