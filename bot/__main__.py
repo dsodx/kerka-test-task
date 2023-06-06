9ec11599eb723be2cfbc4f3bd0053a97724e7ff2
dsodx <dsodx@pm.me>
2023-06-06 11:46:28 +0300

    Add config

--- bot/__main__.py
+++ bot/__main__.py
@@ -4,17 +4,23 @@ import logging
 from aiogram import Bot, Dispatcher
 from aiogram.enums import ParseMode
 
+from .config import config
+
 logger = logging.getLogger(__name__)
 
 
 async def main() -> None:
     logging.basicConfig(level=logging.INFO)
 
-    bot = Bot(token=..., parse_mode=ParseMode.HTML)
+    bot = Bot(token=config.bot_token.get_secret_value(), parse_mode=ParseMode.HTML)
     dp = Dispatcher()
 
+    logger.warning("Running bot..")
     await dp.start_polling(bot)
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    try:
+        asyncio.run(main())
+    except (SystemExit, KeyboardInterrupt):
+        logger.warning("Bot stopped")
