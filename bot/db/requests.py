66aebb07e94b237c5832886aab28fcc6673cbbfb
dsodx <dsodx@pm.me>
2023-06-07 12:05:57 +0300

    Reformat `select()` `to get()`

--- bot/db/requests.py
+++ bot/db/requests.py
@@ -1,3 +1,5 @@
+from typing import Type
+
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
@@ -5,17 +7,17 @@ from .models import User
 
 
 async def add_new_user(session: AsyncSession, /, user_id: int) -> None:
-    user = await session.scalar(select(User).where(User.id == user_id))
+    user = await session.get(User, ident=user_id)
     if user is None:
         user = User(id=user_id)
         session.add(user)
 
 
 async def update_user_balance(session: AsyncSession, /, user_id: int, inc: int) -> None:
-    user = await session.scalar(select(User).where(User.id == user_id))
+    user: Type[User] = await session.get(User, ident=user_id)
     user.balance += inc
 
 
 async def get_user_balance(session: AsyncSession, /, user_id: int) -> float:
-    balance = await session.scalar(select(User.balance).where(User.id == user_id))
+    balance: int = (await session.get(User, ident=user_id)).balance
     return balance / 100
