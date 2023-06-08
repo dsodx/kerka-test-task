04907f45ce37158fbc209e592ab63d98e6450056
dsodx <dsodx@pm.me>
2023-06-08 11:26:19 +0300

    Add admin check

--- bot/handlers/admin.py
+++ bot/handlers/admin.py
@@ -3,11 +3,12 @@ from aiogram.filters import Command
 from aiogram.types import Message
 
 from ..config import Settings
+from ..filters import IsAdminFilter
 from ..ui import get_admin_kb
 
 router = Router()
 
 
-@router.message(Command("admin"))
+@router.message(Command("admin"), IsAdminFilter(is_admin=True))
 async def start_cmd(message: Message, config: Settings) -> None:
     await message.answer("Here", reply_markup=get_admin_kb(config.webapp_url))
--- bot/webapp/routes/verify.py
+++ bot/webapp/routes/verify.py
@@ -1,11 +1,18 @@
-from aiogram.utils.web_app import check_webapp_signature
+from aiogram.utils.web_app import safe_parse_webapp_init_data
 from aiohttp.web import Request, json_response, Response
+
+from ...config import Settings
 
 
 async def verify_init_data(request: Request) -> Response:
-    token = request.app.get("bot_token")
+    token: str = request.app.get("bot_token")
+    config: Settings = request.app.get("config")
     init_data: str = (await request.json()).get("initData", "")
-    res = check_webapp_signature(token=token, init_data=init_data)
+    try:
+        init_data_unsafe = safe_parse_webapp_init_data(token=token, init_data=init_data)
+        res = init_data_unsafe.user.id in config.admin_ids
+    except ValueError:
+        res = False
     return json_response({
         "ok": res
     })
