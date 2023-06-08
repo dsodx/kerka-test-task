81a96e4b2753982aa78fd80010dcb734ce0239a9
dsodx <dsodx@pm.me>
2023-06-08 10:49:10 +0300

    Add new requests

--- bot/db/requests.py
+++ bot/db/requests.py
@@ -1,4 +1,4 @@
-from typing import Type
+from typing import Type, List
 
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -13,7 +13,12 @@ async def add_new_user(session: AsyncSession, /, user_id: int) -> None:
         session.add(user)
 
 
-async def update_user_balance(session: AsyncSession, /, user_id: int, inc: int) -> None:
+async def get_all_users(session: AsyncSession) -> List[User]:
+    users = await session.scalars(select(User).order_by(User.balance.desc()))
+    return [user.as_dict() for user in users]
+
+
+async def update_user_balance_rel(session: AsyncSession, /, user_id: int, inc: int) -> None:
     user: Type[User] = await session.get(User, ident=user_id)
     user.balance += inc
 
@@ -26,3 +31,24 @@ async def get_user_balance(session: AsyncSession, /, user_id: int) -> float:
 async def is_user_banned(session: AsyncSession, /, user_id: int) -> bool:
     user = await session.get(User, ident=user_id)
     return False if user is None else user.banned
+
+
+async def bun_or_unban_user(session: AsyncSession, /, user_id: int) -> bool:
+    user = await session.get(User, ident=user_id)
+    ban = False if user.banned else True
+    user.banned = ban
+    return ban
+
+
+async def update_user_balance(session: AsyncSession, /, user_id: int, new_balance: str) -> float:
+    user = await session.get(User, ident=user_id)
+    try:
+        float(new_balance)
+    except ValueError:
+        return user.balance / 100
+    if new_balance.startswith("+") or new_balance.startswith("-"):
+        user.balance += int(float(new_balance) * 100)
+        return user.balance / 100
+    else:
+        user.balance = int(float(new_balance) * 100)
+        return user.balance / 100
