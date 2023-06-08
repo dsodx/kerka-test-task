9c54a8022d86793aec22c4650f85d34d28ff8690
dsodx <dsodx@pm.me>
2023-06-08 10:48:44 +0300

    Setup new requests

--- bot/webapp/routes/__init__.py
+++ bot/webapp/routes/__init__.py
@@ -1,9 +1,18 @@
 from aiohttp.web import Application
 
 
-def setup_handlers(app: Application) -> None:
+def setup_routers(app: Application) -> None:
     from .root import root_handler
     app.router.add_get("/", root_handler)
 
     from .verify import verify_init_data
     app.router.add_post("/verifyInitData", verify_init_data)
+
+    from .get_users import get_users
+    app.router.add_post("/getUsers", get_users)
+
+    from .ban_user import edit_banned_field
+    app.router.add_post("/editBanField", edit_banned_field)
+
+    from .new_balance import new_user_balance
+    app.router.add_post("/editBalanceField", new_user_balance)
