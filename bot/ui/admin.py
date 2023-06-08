d86d22aba546dbec4030a68633883716b11ecc17
dsodx <dsodx@pm.me>
2023-06-08 19:02:54 +0300

    Add comments to `ui` pkg

--- bot/ui/admin.py
+++ bot/ui/admin.py
@@ -2,6 +2,11 @@ from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, WebAppInfo
 
 
 def get_admin_kb(url: str) -> InlineKeyboardMarkup:
+    """
+    Получить клавиатуру с веб-приложением панели управления
+    :param url: url-адрес веб-приложения
+    :return: объект клавиатуры
+    """
     webapp = WebAppInfo(url=url)
     kb = InlineKeyboardMarkup(inline_keyboard=[
         [InlineKeyboardButton(text="Control Panel", web_app=webapp)]
--- bot/ui/replenish.py
+++ bot/ui/replenish.py
@@ -5,12 +5,21 @@ from aiogram.utils.keyboard import InlineKeyboardBuilder
 
 
 def get_balance_kb() -> InlineKeyboardMarkup:
+    """
+    Получить встроенную клавиатуру для пополнения баланса
+    :return: объект клавиатуры
+    """
     builder = InlineKeyboardBuilder()
     builder.button(text="Пополнить баланс", callback_data="replenish_balance")
     return builder.as_markup()
 
 
 def get_prices(amount: int) -> List[LabeledPrice]:
+    """
+    Получить массив цен пополнения баланса
+    :param amount: сумма пополнения баланса
+    :return: массив цен
+    """
     return [
         LabeledPrice(label="Сумма", amount=amount)
     ]
